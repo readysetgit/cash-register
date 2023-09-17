d8250d185ee8713f2cc797f7c0f5676dc5d23a72
Fahad <fahadfile@gmail.com>
2023-09-16 22:35:14 -0500

    added many changes

--- src/components/AddRemoveMoney.jsx
+++ src/components/AddRemoveMoney.jsx
@@ -1,9 +1,26 @@
-import React, { useState } from 'react';
-import './AddRemoveMoney.scss';
-import {errorToast} from '../services/toast'
+import React, { useState, useEffect } from "react";
+import "./AddRemoveMoney.scss";
+import { errorToast } from "../services/toast";
+import { IconButton } from "@mui/material";
+import AddBox from "@mui/icons-material/AddCircle";
+import Remove from "@mui/icons-material/RemoveCircle";
 
+// Function to format large numbers
+function formatLargeNumber(number) {
+  if (number >= 1e12) {
+    return `${(number / 1e12).toFixed(1)}T`; // Trillion
+  } else if (number >= 1e9) {
+    return `${(number / 1e9).toFixed(1)}B`; // Billion
+  } else if (number >= 1e6) {
+    return `${(number / 1e6).toFixed(1)}M`; // Million
+  } else if (number >= 1e5) {
+    return `${(number / 1e3).toFixed(1)}K`; // Thousand
+  } else {
+    return number.toString();
+  }
+}
 
-function AddRemoveMoney({ onAddMoney,onRemoveMoney, denominations }) {
+function AddRemoveMoney({ onAddMoney, onRemoveMoney, denominations }) {
   const [amountsToAdd, setAmountsToAdd] = useState({
     20: 0,
     10: 0,
@@ -13,6 +30,11 @@ function AddRemoveMoney({ onAddMoney,onRemoveMoney, denominations }) {
   });
 
   const [totalAmount, setTotalAmount] = useState(0);
+
+  useEffect(() => {
+    // Calculate the total amount whenever amountsToAdd changes
+    calculateTotalAmount();
+  }, [amountsToAdd]);
 
   const handleChange = (event) => {
     let { name, value } = event.target;
@@ -30,7 +52,7 @@ function AddRemoveMoney({ onAddMoney,onRemoveMoney, denominations }) {
   const handleIncrease = (denomination) => {
     setAmountsToAdd((prevAmounts) => ({
       ...prevAmounts,
-      [denomination]: parseInt(prevAmounts[denomination], 10) + 1,
+      [denomination]: (parseInt(prevAmounts[denomination], 10) || 0) + 1,
     }));
 
     // Calculate the total amount
@@ -76,7 +98,7 @@ function AddRemoveMoney({ onAddMoney,onRemoveMoney, denominations }) {
   const handleRemoveMoney = () => {
     for (const denomination in amountsToAdd) {
       if (amountsToAdd[denomination] > denominations[denomination]) {
-        errorToast('You cannot remove more bills than available.')
+        errorToast("You cannot remove more bills than available.");
         return;
       }
     }
@@ -93,24 +115,23 @@ function AddRemoveMoney({ onAddMoney,onRemoveMoney, denominations }) {
 
   return (
     <div className="add-money-container">
-      <h2 className="box-title">Select Denominations</h2>
-      <div className="all-labels">
-        {Object.keys(amountsToAdd).map((denomination) => (
-          <div className="row-container" key={denomination}>
-            <span className="x-bills">
-              ${denomination} Bills
+      <div className="top-section">
+        <h2 className="box-title">Select Denominations</h2>
+        <div className="all-labels">
+          {Object.keys(amountsToAdd).map((denomination) => (
+            <div className="row-container" key={denomination}>
+              <span className="x-bills">${denomination} Bills</span>
 
-            </span>
-
-            {/* <button
-                className="btn-decrease"
+              <IconButton
+                aria-label="delete"
+                className="decrease-btn"
                 onClick={() => {
                   handleDecrease(denomination);
                 }}
-                disabled={amountsToAdd[denomination] === 0} // Disable when count is 0
+                disabled={!amountsToAdd[denomination]}
               >
-                -
-              </button> */}
+                <Remove />
+              </IconButton>
               <input
                 className="count-input"
                 type="number"
@@ -121,26 +142,39 @@ function AddRemoveMoney({ onAddMoney,onRemoveMoney, denominations }) {
                 }}
                 min="0" // Prevent negative values
               />
-              {/* <button
-                className="btn-increase"
+              <IconButton
+                aria-label="delete"
+                className="increase-btn"
                 onClick={() => {
                   handleIncrease(denomination);
                 }}
               >
-                +
-              </button> */}
-          </div>
-        ))}
-      </div>
-      <div className='actions-container'>
-        <button className="register-btn add-money-btn" disabled={Object.values(amountsToAdd).every((value) => !value )} onClick={handleAddMoney}>
-            Add Money
-        </button>
-        <button className="register-btn remove-money-btn" disabled={Object.values(amountsToAdd).every((value) => !value )} onClick={handleRemoveMoney}>
-            Remove Money
-        </button>
+                <AddBox />
+              </IconButton>
+            </div>
+          ))}
+        </div>
       </div>
 
+      <div class="bottom-section">
+        <p className="total-money">${formatLargeNumber(totalAmount)}</p>
+        <div className="actions-container">
+          <button
+            className="register-btn add-money-btn"
+            disabled={Object.values(amountsToAdd).every((value) => !value)}
+            onClick={handleAddMoney}
+          >
+            Add Money
+          </button>
+          <button
+            className="register-btn remove-money-btn"
+            disabled={Object.values(amountsToAdd).every((value) => !value)}
+            onClick={handleRemoveMoney}
+          >
+            Remove Money
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
