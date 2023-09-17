a204bf4d5dab9bbdc93374ea19cfb454ff0802a7
Fahad <fahadfile@gmail.com>
2023-09-17 00:47:11 -0500

    visual cleanup

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,19 @@
+import React from 'react';
 import "./Footer.scss";
 
-const Footer = () => {
-    return ( <div className="footer"></div> );
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="container">
+        <p>&copy; 2023 Fahad S. Ahmad</p>
+        <p>
+          <a href="https://www.whoisfahad.com" target="_blank" rel="noopener noreferrer">
+            More About me
+          </a>
+        </p>
+      </div>
+    </footer>
+  );
 }
- 
+
 export default Footer;
